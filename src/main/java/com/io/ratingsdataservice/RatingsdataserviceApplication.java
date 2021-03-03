cd6197035b713ed3bf5c553728cb6f2bb4dd68e0
rajan.ch <rajan.ch@hcl.com>
2021-03-03 09:27:42 +0530

    Initial commit

---
+++ src/main/java/com/io/ratingsdataservice/RatingsdataserviceApplication.java
@@ -0,0 +1,13 @@
+package com.io.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsdataserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingsdataserviceApplication.class, args);
+    }
+
+}
